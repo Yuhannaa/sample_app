489d1a69d9df38a7afa2a12fa3ad2aa1c116c279
Yuhannaa <yuhannaa@gmail.com>
2013-02-19 22:59:17 +0900

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,8 +51,7 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-      describe "in the Users controller" do
-
+      describe "in the Users controller" do        
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_selector('title', text:'Sign in') }
@@ -67,6 +66,58 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+      end
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          sign_in user
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              sign_in user
+            end
+
+            it "should render the default(profile) page" do
+              page.should have_selector('title', text: user.name)
+            end
+          end
+        end
+      end
+
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "submitting a POST request to the Users#create action" do
+        before { post users_path }
+        specify { response.should redirect_to(root_path) }
+      end
+
+      describe "visiting the signup page" do
+        before { visit signup_path }
+        it { should_not have_selector('h1', text: 'Sign up')}
+        it { should_not have_selector('title', text: full_title('Sign up'))}
       end
     end
 
@@ -86,30 +137,26 @@ describe "Authentication" do
       end
     end
 
-    describe "for non-signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-
-      describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email", with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
-
-        describe "after signing in" do
-          it "should render the desired protected page" do
-            page.should have_selector('title', text: 'Edit user')
-          end
-        end
-      end
-    end
-
     describe "as non-admin user" do
       let(:user)  { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
 
       before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        user.save!
+        user.toggle!(:admin)
+        sign_in user
+      end
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,10 +6,19 @@ describe "UserPages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
+
+    describe "micropost" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup" do
@@ -39,7 +48,7 @@ describe "UserPages" do
         fill_in "Name", with: "Example User"
         fill_in "Email", with: "user@example.com"
         fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -138,6 +147,6 @@ describe "UserPages" do
         it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
-
   end
+
 end
